fd13bbbbff325053e7e84034d7ce073ca0474cd2
Ridho Prabowo <ridhoprabwo@gmail.com>
2023-08-03 16:54:05 +0700

    lanjutan belajar

--- base/models.py
+++ base/models.py
@@ -15,7 +15,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -33,5 +33,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-updated", "-created"]
+
     def __str__(self):
-        return self.body[0:5]
+        return self.body[0:50]
--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerPage, name="register"),
     path("", views.home, name="home"),
     path("room/<int:pk>/", views.room, name="room"),
     path("create-room/", views.createRoom, name="create-room"),
     path("update-room/<int:pk>", views.updateRoom, name="update-room"),
     path("delete-room/<int:pk>", views.deleteRoom, name="delete-room"),
+    path("delete-message/<int:pk>", views.deleteMessage, name="delete-message"),
 ]
