c72cc3e8f496781b005a6fce8386a535368d8404
Ridho Prabowo <ridhoprabwo@gmail.com>
2023-08-04 16:55:41 +0700

    lanjutan belajar

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("register/", views.registerPage, name="register"),
     path("", views.home, name="home"),
     path("room/<int:pk>/", views.room, name="room"),
+    path("profile/<int:pk>/", views.userProfile, name="user-profile"),
     path("create-room/", views.createRoom, name="create-room"),
     path("update-room/<int:pk>", views.updateRoom, name="update-room"),
     path("delete-room/<int:pk>", views.deleteRoom, name="delete-room"),
