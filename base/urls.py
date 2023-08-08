bcca871b2fcf3b3b99f1813a393b647dfad1dae0
Ridho Prabowo <ridhoprabwo@gmail.com>
2023-08-09 05:35:11 +0700

    lanjutan

--- base/urls.py
+++ base/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path("room/<int:pk>/", views.room, name="room"),
     path("profile/<int:pk>/", views.userProfile, name="user-profile"),
     path("create-room/", views.createRoom, name="create-room"),
-    path("update-room/<int:pk>", views.updateRoom, name="update-room"),
-    path("delete-room/<int:pk>", views.deleteRoom, name="delete-room"),
-    path("delete-message/<int:pk>", views.deleteMessage, name="delete-message"),
+    path("update-room/<int:pk>/", views.updateRoom, name="update-room"),
+    path("delete-room/<int:pk>/", views.deleteRoom, name="delete-room"),
+    path("delete-message/<int:pk>/", views.deleteMessage, name="delete-message"),
+    path("update-user/", views.updateUser, name="update-user"),
 ]
--- base/views.py
+++ base/views.py
@@ -200,3 +200,8 @@ def deleteMessage(request, pk):
         return redirect("home")
 
     return render(request, "base/delete.html", {"obj": message})
+
+
+@login_required(login_url="login")
+def updateUser(request):
+    return render(request, "base/update-user.html")
