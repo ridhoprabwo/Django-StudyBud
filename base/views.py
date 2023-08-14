ae34a711e2a8729347ac5721bf54796ddfe74048
Ridho Prabowo <ridhoprabwo@gmail.com>
2023-08-14 11:46:40 +0700

    Django Studybud

--- base/views.py
+++ base/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -24,15 +22,15 @@ def loginPage(request):
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username").lower()
+        email = request.POST.get("email").lower()
         password = request.POST.get("password")
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "User does not exist")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
@@ -45,10 +43,10 @@ def loginPage(request):
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -56,7 +54,7 @@ def registerPage(request):
             login(request, user)
             return redirect("home")
         else:
-            messages.error(request, "An error occured during registration")
+            messages.error(request, "The Password is weak!")
 
     return render(request, "base/login_register.html", {"form": form})
 
@@ -208,12 +206,12 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == "POST":
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect("user-profile", pk=user.id)
 
-    return render(request, "base/update-user.html", {"form": form})
+    return render(request, "base/update_user.html", {"form": form})
 
 
 def topicsPage(request):
